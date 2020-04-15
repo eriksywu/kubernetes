1b5c675781c71a9ab22626e76d1ecc21208597b2
Erik Wu <erw@microsoft.com>
2020-04-15 16:03:07 -0700

    don't delete pod

--- test/e2e/autoscaling/dns_autoscaling.go
+++ test/e2e/autoscaling/dns_autoscaling.go
@@ -265,9 +265,9 @@ var _ = SIGDescribe("[ProportionalScaling] DNS horizontal autoscaling", func() {
 		err = updateDNSScalingConfigMap(c, packDNSScalingConfigMap(packLinearParams(&DNSParams1)))
 		framework.ExpectNoError(err)
 
-		ginkgo.By("Delete the autoscaler pod for kube-dns")
+		/*ginkgo.By("Delete the autoscaler pod for kube-dns")
 		err = deleteDNSAutoscalerPod(c)
-		framework.ExpectNoError(err)
+		framework.ExpectNoError(err)*/
 
 		ginkgo.By("Wait for kube-dns scaled to expected number")
 		getExpectReplicasLinear = getExpectReplicasFuncLinear(c, &DNSParams1)
